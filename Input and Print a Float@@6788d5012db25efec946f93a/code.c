54f0949aacd18da813d611f49b820149b36023d1
SyntaxBugger <tuhin4174@gmail.com>
2025-01-17 11:15:01 +0530

    Solution to Input and Print a Float

---
+++ Input and Print a Float@@6788d5012db25efec946f93a/code.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+char* welcome() {
+    return "Welcome to Code Radar!";
+    int a;
+    scanf(&f,a);
+    return a;
+}
+
+int main() {
+    printf("%s", welcome());
+    return 0;
+}
