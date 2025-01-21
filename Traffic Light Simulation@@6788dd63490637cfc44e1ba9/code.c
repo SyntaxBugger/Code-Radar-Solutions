ba2c7654c02157bfff024f151e08b47312863ece
SyntaxBugger <tuhin4174@gmail.com>
2025-01-21 21:37:28 +0530

    Solution to Traffic Light Simulation

---
+++ Traffic Light Simulation@@6788dd63490637cfc44e1ba9/code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    char a;
+    scanf("%d",%a);
+    if(a=='R'){
+        printf("Stop");
+    }else if(a=='G'){
+        printf("Go");
+    }else if(a=='Y'){
+        printf("Slow Down");
+    }else{
+        printf("Invalid Input");
+    }
+    return 0;
+}
