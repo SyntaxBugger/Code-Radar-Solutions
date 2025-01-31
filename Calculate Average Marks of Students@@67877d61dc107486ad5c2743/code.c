b5462518c8eccc80d380ce36738df37ca64c64e4
SyntaxBugger <tuhin4174@gmail.com>
2025-01-31 11:57:07 +0530

    Solution to Calculate Average Marks of Students

--- Calculate Average Marks of Students@@67877d61dc107486ad5c2743/code.c
+++ Calculate Average Marks of Students@@67877d61dc107486ad5c2743/code.c
@@ -14,6 +14,6 @@ int main() {
         scanf("%d %s %f",&s[i].rno,s[i].name,&s[i].marks);
         sum+=s[i].marks;
     }
-    printf("Average Marks: %.2f",sum+/n);
+    printf("Average Marks: %.2f",sum/n);
     return 0;
 }
