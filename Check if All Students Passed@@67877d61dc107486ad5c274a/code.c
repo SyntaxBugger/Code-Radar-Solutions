1ca607e006f8a19af4fd3aff9a8eb0412e8c6b60
SyntaxBugger <tuhin4174@gmail.com>
2025-01-31 21:42:37 +0530

    Solution to Check if All Students Passed

--- Check if All Students Passed@@67877d61dc107486ad5c274a/code.c
+++ Check if All Students Passed@@67877d61dc107486ad5c274a/code.c
@@ -12,12 +12,15 @@ int main() {
     for(int i=0; i<n; i++){
         scanf("%d %s %f",&s1[i].rno,s1[i].name,&s1[i].marks);
     }
+    int flag=0
     for(int i=0; i<n; i++){
         if(s1[i].marks==50){
             printf("All Passed");
-        }else{
-            printf("Not All Passed");
+            flag=1;
         }
+    }
+    if(flag==0){
+        printf("Not All Passed");
     }
     return 0;
 }
