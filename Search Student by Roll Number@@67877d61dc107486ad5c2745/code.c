24c8508e4449191faf1f9c6118d3e69ea979f33f
SyntaxBugger <tuhin4174@gmail.com>
2025-01-31 21:10:07 +0530

    Solution to Search Student by Roll Number

---
+++ Search Student by Roll Number@@67877d61dc107486ad5c2745/code.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+struct student{
+    int rno;
+    char name[69];
+    float marks;
+};
+int main() {
+    int n;
+    scanf("%d",&n);
+    struct student s1[n];
+    for(int i=0;i<n; i++){
+        scanf("%d %s %f",&s1[i].rno,s1[i].name,&s1[i].marks);
+    }
+    int search;
+    scanf("%d",&search)
+    for(int i=0; i<n; i++){
+        if(s1[i].rno==search){
+            printf("Roll Number: %d,",s1[i].rno);
+            printf("Name: %s,",s1[i].name);
+            printf("Marks: %.2f",s1[i].marks);
+        }else{
+            printf("Student not found");
+        }
+    }
+    return 0;
+}
