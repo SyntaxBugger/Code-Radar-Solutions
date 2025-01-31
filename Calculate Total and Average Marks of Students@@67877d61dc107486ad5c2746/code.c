c1f88e3d6a4ece65424c5d1e9c3ec605b83e76ec
SyntaxBugger <tuhin4174@gmail.com>
2025-01-31 20:03:11 +0530

    Solution to Calculate Total and Average Marks of Students

---
+++ Calculate Total and Average Marks of Students@@67877d61dc107486ad5c2746/code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct student{
+    int rno;
+    char name[60];
+    float marks;
+};
+int main() {
+    int n;
+    int sum=0;
+    scanf("%d",&n);
+    struct student s1;
+    for(int i=0; i<n; i++){
+        scanf("%d %s %f",&s1[i].rno,s1[i].name,&s1[i].marks);
+        sum+=s1[i].marks;
+    }
+    printf("Total Marks: %d",sum);
+    printf("Average Marks: %d",sum/n);
+    return 0;
+}
