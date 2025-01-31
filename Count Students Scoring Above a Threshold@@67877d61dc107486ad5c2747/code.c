2749607ea064ae7175ac67ca387372bd973e2ec6
SyntaxBugger <tuhin4174@gmail.com>
2025-01-31 20:46:36 +0530

    Solution to Count Students Scoring Above a Threshold

---
+++ Count Students Scoring Above a Threshold@@67877d61dc107486ad5c2747/code.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+struct student{
+    int rno;
+    char name[78];
+    float marks;
+}
+int main() {
+    int n;
+    scanf("%d",&n);
+    int cnt=0;
+    struct student s1[n];
+    for(int i=0; i<n; i++){
+        scanf("%d %s %f",&s1[i].rno,s1[i].name,s1[i].marks);
+    }
+    float thold;
+    scanf("%f",%thold);
+    for(int i=0; i<n; i++){
+        if(s1[i].marks>thold){
+            cnt++;
+        }
+    }
+    printf("%d",cnt);
+    return 0;
+}
