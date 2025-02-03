8d3def339a8916575362be0a32a52fd333ffc2e0
SyntaxBugger <tuhin4174@gmail.com>
2025-02-03 11:28:03 +0530

    Solution to Calculate Employee Bonus

---
+++ Calculate Employee Bonus@@67877d61dc107486ad5c274f/code.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+struct bonus{
+    int id;
+    char name[69];
+    float sal;
+}
+int main() {
+    int n;
+    scanf("%d",&n);
+    struct bonus b1;
+    for(int i=0; i<n; i++){
+        scanf("%d %s %f",&b1[i].id,b1[i].name,&b1[i].sal);
+    }
+    for(int i=0; i<n; i++){
+        float bonus;
+        if(sal<50000){
+            bonus=(10/100)*b1[i].sal;
+            printf("Employee ID: %d, Name: %s, Bonus: %.2f",b1[i].id,b1[i].name,bonus);
+        }else{
+            bonus=(5/100)*b1[i].sal;
+            printf("Employee ID: %d, Name: %s, Bonus: %.2f",b1[i].id,b1[i].name,bonus);
+        }
+    }
+    return 0;
+}
