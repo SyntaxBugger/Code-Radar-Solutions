088da1c1af4ddb0ff54f929855d48dd46f24df61
SyntaxBugger <tuhin4174@gmail.com>
2025-02-06 22:10:34 +0530

    Solution to Streaming Service Subscription Management

--- Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
+++ Streaming Service Subscription Management@@67877d61dc107486ad5c2752/code.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-struct movieTicket{
-    char movie[69];
+struct subs{
+    char name[69];
     char type[69];
     float price;
 };
@@ -9,20 +9,24 @@ struct movieTicket{
 int main() {
     int n;
     float sum1=0, sum2=0, sum3=0;
+    int bcnt=0, scnt=0, pcnt=0;
     scanf("%d",&n);
-    struct movieTicket m1[n];
+    struct subs m1[n];
     for(int i=0; i<n; i++){
-        scanf("%s %s %f",m1[i].movie,m1[i].type,&m1[i].price);
+        scanf("%s %s %f",m1[i].name,m1[i].type,&m1[i].price);
     }
     for(int i=0; i<n; i++){
-        if(strcmp(m1[i].type, "Standard") == 0){
+        if(strcmp(m1[i].type, "Basic") == 0){
             sum1+=m1[i].price;
-        }else if(strcmp(m1[i].type, "Premium") == 0){
+            bcnt++;
+        }else if(strcmp(m1[i].type, "Standard") == 0){
             sum2+=m1[i].price;
+            scnt++;
         }else{
             sum3+=m1[i].price;
+            pcnt++;
         }
     }
-    printf("Standard: %.2f, Premium: %.2f, VIP: %.2f",sum1,sum2,sum3);
+    printf("Basic: %d Users, Revenue: %.2f; Standard: %d Users, Revenue: %.2f;, VIP: %d Users, Revenue: %.2f",bcnt,sum1,scnt,sum2,pcnt,sum3);
     return 0;
 }
