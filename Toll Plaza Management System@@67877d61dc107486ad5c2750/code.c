81e4aae7d8a7628f9ce3eaa5748c237e06353462
SyntaxBugger <tuhin4174@gmail.com>
2025-02-06 21:23:26 +0530

    Solution to Toll Plaza Management System

---
+++ Toll Plaza Management System@@67877d61dc107486ad5c2750/code.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+struct toll{
+    char plate[69];
+    char car[69];
+    float tollamt;
+};
+
+int main() {
+    int n;
+    float sum1=0, sum2=0, sum3=0;
+    scanf("%d",&n);
+    struct toll t1[n];
+    for(int i=0; i<n; i++){
+        scanf("%s %s %f",t1[i].plate,t1[i].car,&t1[i].tollamt);
+    }
+    for(int i=0; i<n; i++){
+        if(t1[i].car=='Cars'){
+            sum1+=t1[i].tollamt;
+        }else if(t1[i].car=='Truck'){
+            sum2+=t1[i].tollamt;
+        }else{
+            sum3+=t1[i].tollamt;
+        }
+    }
+    printf("Cars: %.2f, Trucks: %.2f, Bikes: %.2f",sum1,sum2,sum3);
+    return 0;
+}
