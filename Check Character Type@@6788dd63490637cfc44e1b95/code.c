c32ee873fa7e4fc824f48fac347d399887e6132d
SyntaxBugger <tuhin4174@gmail.com>
2025-02-07 22:40:36 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
-
-char* welcome() {
-    return "Welcome to Code Radar!";
-}
+#include <ctype.h> 
 
 int main() {
-    printf("%s", welcome());
+    char c;
+    scanf("%c", &c);
+    c = tolower(c);
+
+    if (isalpha(c)) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+            printf("The character is a vowel.\n");
+        } else {
+            printf("The character is a consonant.\n");
+        }
+    } else if (isdigit(c)) {
+        printf("The character is a digit.\n");
+    } else {
+        printf("The character is a special character.\n");
+    }
+
     return 0;
 }
