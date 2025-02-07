d6ab6c2ce0f488278e310f1073ed6118c10a4d7f
SyntaxBugger <tuhin4174@gmail.com>
2025-02-07 22:35:00 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    char c;
+    scanf("%d %d %c",&a,&b,c);
+    if(c=='+'){
+        printf("%d",a+b);
+    }else if(c=='-'){
+        printf("%d",a-b);
+    }else if(c=='*'){
+        printf("%d",a*b);
+    }else{
+        if(b==0){
+            printf("Error");
+        }else{
+            printf("%d",a/b);
+        }
+    }
+    return 0;
+}
