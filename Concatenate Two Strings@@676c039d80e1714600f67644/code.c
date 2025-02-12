43fc377047df5a8180ea69a439f758cdd1d06f39
SyntaxBugger <tuhin4174@gmail.com>
2025-02-12 11:27:18 +0530

    Solution to Concatenate Two Strings

--- Concatenate Two Strings@@676c039d80e1714600f67644/code.c
+++ Concatenate Two Strings@@676c039d80e1714600f67644/code.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
 #include <string.h>
-
-void removeNewline(char *str) {
-    str[strcspn(str, "\n")] = '\0'; // Replace newline with null terminator
-}
-
 int main() {
     char a[100], b[20];
-
-    // Read input strings
     fgets(a, sizeof(a), stdin);
     fgets(b, sizeof(b), stdin);
-
-    // Remove trailing newlines
-    removeNewline(a);
-    removeNewline(b);
-
-    // Concatenate and print
+    str[strcspn(a, "\n")] = '\0';
+    str[strcspn(b, "\n")] = '\0';
     strcat(a, b);
     printf("%s\n", a);
     
